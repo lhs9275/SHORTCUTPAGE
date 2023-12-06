b19bd775fe6e8e7f1ea8abb8a5825580c379a856
whoMI <98660600+lhs9275@users.noreply.github.com>
2023-12-06 15:53:38 +0900

    Update ING.js

--- ING.js
+++ ING.js
@@ -52,7 +52,7 @@ function getClock() {
    const minutes = String(date.getMinutes()).padStart(2, "0");
    const seconds = String(date.getSeconds()).padStart(2, "0");
 
-   clock.innerText = `${hours}:${minutes}:${seconds} `;
+   clock.innerText = `${hours}시${minutes}분${seconds}초 `;
 }
 
 
