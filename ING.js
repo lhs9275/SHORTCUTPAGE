8912b5d215f09da94401036e30feeb176967d16a
whoMI <98660600+lhs9275@users.noreply.github.com>
2023-12-07 16:22:20 +0900

    Update ING.js

--- ING.js
+++ ING.js
@@ -62,21 +62,20 @@ setInterval(getClock, 1000);
 
 
 //슬라이드 
+const images = document.querySelectorAll('.img');
+let current = 0;
 
-const sliderWrap = document.querySelector(".slider__wrap");
-const sliderImg = sliderWrap.querySelector(".slider__img");
-const slider = sliderWrap.querySelectorAll(".slider");
-let currentIndex = 0;               // 현재 보이는 이미지
-let sliderCount = slider.length;    // 이미지 갯수
-let sliderInterval = 3000;          // 이미지 변경 간격 시간
+function showSlide() {
+  for(let i=0; i<images.length; i++) {
+    images[i].classList.remove('on');
+  }
+  current++;
+  if(current > images.length) {
+    current = 1;
+  }
+  images[current - 1].classList.add('on');
+  setTimeout(showSlide, 2000);
+}
 
-setInterval(() => {
-  let nextIndex =(currentIndex+1)%sliderCount;
+showSlide();
 
-  slider[currentIndex].style.opacity = "0";
-  slider[nextIndex].style.opacity = "1";
-
-  currentIndex = nextIndex;
-
-  console.log(currentIndex);
-}, sliderInterval);
