7b398908a5a72a5d45e90dd4f9c33e3ad79dadc3
whoMI <98660600+lhs9275@users.noreply.github.com>
2023-12-07 11:24:04 +0900

    Update ING.js

--- ING.js
+++ ING.js
@@ -65,8 +65,7 @@ setInterval(getClock, 1000);
 
 const sliderWrap = document.querySelector(".slider__wrap");
 const sliderImg = sliderWrap.querySelector(".slider__img");
-const slider = sliderWrap.querySelectorAll(".slider")
-
+const slider = sliderWrap.querySelectorAll(".slider");
 let currentIndex = 0;               // 현재 보이는 이미지
 let sliderCount = slider.length;    // 이미지 갯수
 let sliderInterval = 3000;          // 이미지 변경 간격 시간
